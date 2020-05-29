bb40ccf0187d8099d0ed09467f6b57a00564ad40
Dies1rae <unluck.bastard@gmail.com>
2020-05-29 12:02:24 +0300

    some fix

--- snake/snake.cpp
+++ snake/snake.cpp
@@ -2,43 +2,31 @@
 #include <iostream>
 #include "snake_part.h"
 
-void snake::move_to_direction(int W) {
-	if (W == 1) {
-		this->body->move(this->body->get_SP_coord()[0] - 1, this->body->get_SP_coord()[1], 0);
-	}
-	if (W == 4) {
-		this->body->move(this->body->get_SP_coord()[0] + 1, this->body->get_SP_coord()[1], 0);
-	}
-	if (W == 2) {
-		this->body->move(this->body->get_SP_coord()[0], this->body->get_SP_coord()[1] - 1, 0);
-	}
-	if (W == 3) {
-		this->body->move(this->body->get_SP_coord()[0], this->body->get_SP_coord()[1] + 1, 0);
-	}
-}
-void snake::move_to_direction_growup(int W) {
-	this->length++;
-	if (W == 1) {
-		this->body->move(this->body->get_SP_coord()[0] - 1, this->body->get_SP_coord()[1], 1);
-	}
-	if (W == 4) {
-		this->body->move(this->body->get_SP_coord()[0] + 1, this->body->get_SP_coord()[1], 1);
-	}
-	if (W == 2) {
-		this->body->move(this->body->get_SP_coord()[0], this->body->get_SP_coord()[1] - 1, 1);
-	}
-	if (W == 3) {
-		this->body->move(this->body->get_SP_coord()[0], this->body->get_SP_coord()[1] + 1, 1);
-	}
-}
-void  snake::set_way_dir(int W, bool G) {
+void snake::move_to_direction_grow(int W, bool G) {
 	if (G) {
-		snake::move_to_direction_growup(W);
+		this->length++;
 	}
-	if (!G) {
-		snake::move_to_direction(W);
+	this->way = W;
+	if (W == 1) {
+		this->body->move(this->body->get_SP_coord()[0] - 1, this->body->get_SP_coord()[1], G);
+	}
+	if (W == 4) {
+		this->body->move(this->body->get_SP_coord()[0] + 1, this->body->get_SP_coord()[1], G);
+	}
+	if (W == 2) {
+		this->body->move(this->body->get_SP_coord()[0], this->body->get_SP_coord()[1] - 1, G);
+	}
+	if (W == 3) {
+		this->body->move(this->body->get_SP_coord()[0], this->body->get_SP_coord()[1] + 1, G);
 	}
 }
+int snake::get_length() {
+	return this->length;
+}
+int snake::get_way() {
+	return this->way;
+}
+
 	/* move patterns
 		1
 	2	0	3
