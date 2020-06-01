ac470ad322a04b12782741bbd37bec94487f0641
Николай Полтавский <unluck.bastard@gmail.com>
2020-06-02 00:54:12 +0300

    position step1

--- snake/game.cpp
+++ snake/game.cpp
@@ -18,19 +18,30 @@ void game::draw(sf::RenderTarget& target, sf::RenderStates states) const {
 	shape.setFillColor(GameField);
 	shape.setSize(sf::Vector2f(pxCELL_SIZE, pxCELL_SIZE));
 
-	sf::Text snake("*", font, 15);
-	snake.setFillColor(sf::Color::Black);
+
+	sf::Text snakehead("*", font, 15);
+	snakehead.setFillColor(sf::Color::White);
+	sf::Text snakebody("+", font,15);
+	snakebody.setFillColor(sf::Color::Black);
 
 
 	for (int ptr = 0; ptr < fieldARRAY_SIZE; ptr++) {	
 		sf::Vector2f position(ptr % fieldSIZE * pxCELL_SIZE, ptr / fieldSIZE * pxCELL_SIZE);
-		snake.setPosition(position.x+0.5, position.y-1);
+		snakehead.setPosition(position.x + 0.5, position.y - 1);
+		snakebody.setPosition(position.x + 0.5, position.y -5);
 		shape.setPosition(position);
 		target.draw(shape, states);
+
 		for (int ptr1 = 0; ptr1 < SNAKEMAIN->get_length()*2;ptr1++) {
-			if (SNAKEMAIN->get_coord()[ptr1] == ptr) {
-				std::cout << SNAKEMAIN->get_coord()[ptr1] << std::endl;
-				target.draw(snake, states);
+			if(ptr1 % 2 != 0){
+				if (SNAKEMAIN->get_coord()[ptr1] == ptr) {
+					if (ptr1 == 1) {
+						target.draw(snakehead, states);
+					}
+					else {
+						target.draw(snakebody, states);
+					}
+				}
 			}
 		}
 		
